27cbc73a781ad548adf6210dad1715725a098911
Gabriel Carvalho <gabrielcarvalhos.snt@gmail.com>
2025-09-04 19:51:11 -0300

    add second solution minor and position problem

--- 1180b.c
+++ 1180b.c
@@ -1,23 +1,26 @@
 #include <stdio.h>
 
 int main() {
-    int n, menor = 9999, posicao;
+    int n, menor = 9999, posicao = 0, *p;
 
     scanf("%d", &n);
 
     int x[n];
+    p = x;
 
     for (int i = 0; i < n; i++) {
-        scanf("%d", &x[i]);
+        scanf("%d", p);
 
-        if (x[i] < menor) {
-            menor = x[i];
+        if (*p < menor) {
+            menor = *p;
             posicao = i;
         }
+
+        p++;
     }
 
     printf("Menor valor: %d\n", menor);
     printf("Posicao: %d\n", posicao);
-
+    
     return 0;
 }
